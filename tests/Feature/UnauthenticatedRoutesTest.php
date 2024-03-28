ac0e6ba4bd16ebdd75638f69531f3e9e95448108
regis <amaral.regis@gmail.com>
2024-03-28 08:59:56 -0300

    Testes de rotas

---
+++ tests/Feature/UnauthenticatedRoutesTest.php
@@ -0,0 +1,15 @@
+<?php
+
+test('retorna 404 para método get na rota /api/register', function () {
+    $response = $this->get('/api/register');
+    $response->assertStatus(404);
+});
+
+test('retorna 404 para usuário não autenticado acessando a rota /api/user', function () {
+    $response = $this->get('/api/user');
+    $response->assertStatus(404);
+});
+
+test('', function (){
+
+});
