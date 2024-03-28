811ad792924a1c69936837a6cd4bb2cb7a627bb0
regis <amaral.regis@gmail.com>
2024-03-28 15:12:59 -0300

    Novas mensagens de erro no login

--- lang/en/messages.php
+++ lang/en/messages.php
@@ -3,4 +3,6 @@
 return [
     'store.success' => 'Registered successfully.',
     'update.success' => 'Updated successfully.',
+    'login.invalid_credentials' => 'Incorrect email or password.',
+    'login.account_inactive' => 'Login disabled.',
 ];
--- lang/pt_BR/messages.php
+++ lang/pt_BR/messages.php
@@ -3,4 +3,6 @@
 return [
     'store.success' => 'Cadastrado com sucesso.',
     'update.success' => 'Atualizado com sucesso.',
+    'login.invalid_credentials' => 'Email ou senha incorreta.',
+    'login.account_inactive' => 'Login desativado.',
 ];
